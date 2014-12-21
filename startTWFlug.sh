bde97a4536181b1a9b641a5725570066b49ba636
Thomas Wenzlaff <karotz@wenzlaff.de>
2014-12-21 19:05:38 +0000

    Script ohne Psw.
    git-svn-id: file:///Users/thomaswenzlaff/svn/repos/java/TWFlug/trunk@2040 58caa775-73a5-0844-8a3b-92c2738f582a

--- startTWFlug.sh
+++ startTWFlug.sh
@@ -1,1 +1,1 @@
-java -jar twflug.jar -n --copy --ip pi-bplus --copy --ziel-ip pi-home --ziel-user pi --ziel-passwort PASSWORT --ziel-datei /home/pi/fhem/log/flugdaten-test-mac.log --min-count 1 -max-count 60 --window-width 300 --window-height 300 &
+java -jar twflug.jar -n --copy --ip pi-bplus --copy --ziel-ip pi-home --ziel-user pi --ziel-passwort PASSWORT  &
