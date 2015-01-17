115a60ea9d90f278e672b29b16425fa1cc7f46e0
Thomas Wenzlaff <karotz@wenzlaff.de>
2015-01-17 19:48:50 +0000

    Beta Grafik Dialog
    git-svn-id: file:///Users/thomaswenzlaff/svn/repos/java/TWFlug/trunk@2074 58caa775-73a5-0844-8a3b-92c2738f582a

---
+++ src/test/java/de/wenzlaff/twflug/be/TestPositionen.java
@@ -0,0 +1,41 @@
+package de.wenzlaff.twflug.be;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Test Positionen und Langenhagen.
+ * 
+ * @author Thomas Wenzlaff
+ * @version 0.1
+ * @since 11.11.2014
+ */
+public class TestPositionen {
+
+	private List<Position> positionen = new ArrayList<Position>();
+	/**
+	 * Die Home Position (Langenhagen).
+	 */
+	public static final Position HOME = new Position(52.4548, 9.7123);
+
+	public TestPositionen() {
+		// links unten
+		Position p1 = new Position(52.3781, 9.0037);
+		// rechts unten
+		Position p2 = new Position(52.8197, 9.0248);
+		// links oben
+		Position p3 = new Position(52.3133, 10.1586);
+		positionen.add(p1);
+		positionen.add(p2);
+		positionen.add(p3);
+	}
+
+	public List<Position> getPositionen() {
+		return positionen;
+	}
+
+	public void setPositionen(List<Position> positionen) {
+		this.positionen = positionen;
+	}
+
+}
