8c7533d8abc48972c2b10f9ff30258b50ccc0a4f
Thomas Wenzlaff <karotz@wenzlaff.de>
2015-06-28 06:06:44 +0000

    Mit cmd Schnittstelle
    git-svn-id: file:///Users/thomaswenzlaff/svn/repos/java/TWFlug/trunk@2142 58caa775-73a5-0844-8a3b-92c2738f582a

--- src/main/java/de/wenzlaff/twflug/ServiceThingSpeak.java
+++ src/main/java/de/wenzlaff/twflug/ServiceThingSpeak.java
@@ -1,7 +1,5 @@
 package de.wenzlaff.twflug;
 
-import java.util.Date;
-
 import com.angryelectron.thingspeak.Channel;
 import com.angryelectron.thingspeak.Entry;
 import com.angryelectron.thingspeak.ThingSpeakException;
@@ -33,27 +31,35 @@ public class ServiceThingSpeak {
 	/**
 	 * Testmethode.
 	 * 
+	 * Aufruf: ServiceThingSpeak Feld1 ApiWriteKey ChannelId Status
+	 * 
 	 * @param args
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
+		if (args.length < 3) {
+			System.out.println("Aufruf: ServiceThingSpeak Feld1 ApiWriteKey ChannelId Status");
+		}
 
-		String anzahlFlugzeuge = "25";
-		String apiWriteKey = "eintragen";
-		Integer channelId = 44177;
+		String anzahlFlugzeuge = args[0];
+		String apiWriteKey = args[1];
+		Integer channelId = Integer.valueOf(args[2]);
+		String status = "";
+		if (args != null) {
+			status = args[3];
+		}
 
-		send(anzahlFlugzeuge, apiWriteKey, channelId);
+		send(anzahlFlugzeuge, apiWriteKey, channelId, status);
 	}
 
-	public static void send(String anzahlFlugzeuge, String apiWriteKey, Integer channelId) throws UnirestException, ThingSpeakException {
+	public static void send(String anzahlFlugzeuge, String apiWriteKey, Integer channelId, String status) throws UnirestException, ThingSpeakException {
 
 		Channel channel = new Channel(channelId, apiWriteKey);
 
 		Entry entry = new Entry();
 		entry.setField(1, anzahlFlugzeuge);
-		String nachricht = "Update: " + anzahlFlugzeuge + " Flugzeuge erfasst am " + new Date(System.currentTimeMillis());
-		entry.setStatus(nachricht);
-		System.out.println(nachricht);
+		entry.setStatus(status);
+		System.out.println(status);
 		channel.update(entry);
 
 		System.out.println("Update ok");
