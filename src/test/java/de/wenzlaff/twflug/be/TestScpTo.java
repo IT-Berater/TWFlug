8183cfcc3db86687bd28e29acaba96cdc9a91f5f
Thomas Wenzlaff <bit@wenzlaff.de>
2024-12-13 19:17:25 +0100

    Version 1.1.0 mit neuen Jars

--- src/test/java/de/wenzlaff/twflug/be/TestScpTo.java
+++ src/test/java/de/wenzlaff/twflug/be/TestScpTo.java
@@ -1,25 +1,5 @@
 package de.wenzlaff.twflug.be;
 
-/*
- * #%L
- * twflug
- * %%
- * Copyright (C) 2015 Thomas Wenzlaff
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -155,8 +135,7 @@ public class TestScpTo {
 		String passwd;
 		JTextField passwordField = new JPasswordField(20);
 
-		final GridBagConstraints gbc = new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0),
-				0, 0);
+		final GridBagConstraints gbc = new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
 
 		private Container panel;
 
